d8e412a7c4cfda2675a5078e84c7676ecbfe88ee
Mason Staatz <mason_staatz1@baylor.edu>
2022-03-02 15:39:29 -0600

    Exam edits

--- CourseClass.py
+++ CourseClass.py
@@ -1,11 +1,9 @@
 class Course:
-
-    def __init__(self,name,crn,seats,status):
+    def __init__(self, name, crn, seats, status):
         self.__name = name
         self.__crn = crn
         self.__seats = seats
         self.__status = status
-
 
     def get_name(self):
         return self.__name
@@ -19,24 +17,19 @@ class Course:
     def get_status(self):
         return self.__status
 
-
     def update_seat_count(self):
         if self.__seats == 0:
-            self.__status = 'closed'
+            self.__status = "closed"
         else:
             self.__seats -= 1
             if self.__seats == 0:
-               self.__status = 'closed' 
+                self.__status = "closed"
+
 
 class Register:
-
-    def __init__(self,name,crn):
+    def __init__(self, name, crn):
         self.__studentname = name
         self.__crn = crn
 
     def get_name(self):
         return self.__studentname
-    
-
-
-            
